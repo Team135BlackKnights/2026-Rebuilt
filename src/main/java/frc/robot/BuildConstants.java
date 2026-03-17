83751d087c6bb99c9f726e3fcc4fd8b983fb8ea1
Eaglegrant <grantman522@gmail.com>
2026-03-17 13:17:22 -0400

    changesFromMish

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
    public static final String MAVEN_GROUP = "";
    public static final String MAVEN_NAME = "2026-Rebuilt";
    public static final String VERSION = "unspecified";
-   public static final int GIT_REVISION = 1116;
-   public static final String GIT_SHA = "2a9017d96bd0c05c5fd5e78b57d1a39bf7b35c0e";
-   public static final String GIT_DATE = "2026-03-08 08:47:36 EDT";
+   public static final int GIT_REVISION = 1118;
+   public static final String GIT_SHA = "8a2e431ec708c0bdbc890c79183daf905ef4034e";
+   public static final String GIT_DATE = "2026-03-08 14:34:28 EDT";
    public static final String GIT_BRANCH = "2026-Robot-Code";
-   public static final String BUILD_DATE = "2026-03-08 11:33:24 EDT";
-   public static final long BUILD_UNIX_TIME = 1772984004536L;
+   public static final String BUILD_DATE = "2026-03-08 14:54:52 EDT";
+   public static final long BUILD_UNIX_TIME = 1772996092597L;
    public static final int DIRTY = 1;
 
    private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/intake/frontRollers/FrontRollers.java
+++ src/main/java/frc/robot/subsystems/intake/frontRollers/FrontRollers.java
@@ -23,7 +23,7 @@ public class FrontRollers extends GenericRollerSystem<FrontRollers.Goal> {
         IDLING(new LoggableTunedNumber("Intake/FrontRollers/IdlingVoltage", 0.0,
                 Constants.TuningConstants.isTuningIntake)),
         INTAKING(
-                new LoggableTunedNumber("Intake/FrontRollers/IntakingVoltage", 8.0,
+                new LoggableTunedNumber("Intake/FrontRollers/IntakingVoltage", 7.0,
                         Constants.TuningConstants.isTuningIntake)),
         SHOOTING(new LoggableTunedNumber("Intake/FrontRollers/ShootingVoltage", 10.0,
                 Constants.TuningConstants.isTuningIntake)),
