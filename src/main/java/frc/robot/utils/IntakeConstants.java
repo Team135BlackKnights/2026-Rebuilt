4cdfa97f1e29384cb7c3b13eb9cc3ea5c0068161
Vazgen Manukyan <vmanukyan135@phm.k12.in.us>
2026-04-03 15:53:10 -0400

    Update at 'Fri Apr 03 15:53:10 EDT 2026'

--- src/main/java/frc/robot/utils/IntakeConstants.java
+++ src/main/java/frc/robot/utils/IntakeConstants.java
@@ -7,7 +7,7 @@ public class IntakeConstants {
     public static final int intakeMotorID = 40; 
     public static final int frontRollersMotorID = 42;
     public static final int intakeCurrentLimit = 60; // amps
-    public static final int frontRollersCurrentLimit = 30; // amps
+    public static final int frontRollersCurrentLimit = 60; // amps
     public static final boolean intakeInverted = false;
     public static final boolean frontRollersInverted = true;
     public static final DCMotor intakeArmMotor = DCMotor.getKrakenX60Foc(1);
