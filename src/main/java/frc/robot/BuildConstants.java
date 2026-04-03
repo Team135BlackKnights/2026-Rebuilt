f396b2798bca69804588ddb3106ce484e4328e74
Vazgen Manukyan <vmanukyan135@phm.k12.in.us>
2026-04-03 16:34:10 -0400

    Update at 'Fri Apr 03 16:34:10 EDT 2026'

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
    public static final String MAVEN_GROUP = "";
    public static final String MAVEN_NAME = "2026-Rebuilt";
    public static final String VERSION = "unspecified";
-   public static final int GIT_REVISION = 1173;
-   public static final String GIT_SHA = "4cdfa97f1e29384cb7c3b13eb9cc3ea5c0068161";
-   public static final String GIT_DATE = "2026-04-03 15:53:10 EDT";
+   public static final int GIT_REVISION = 1176;
+   public static final String GIT_SHA = "4bd3510ad3c4548c004decee77b35c5b0ba9f285";
+   public static final String GIT_DATE = "2026-04-03 15:58:21 EDT";
    public static final String GIT_BRANCH = "event_2026wilax";
-   public static final String BUILD_DATE = "2026-04-03 15:53:12 EDT";
-   public static final long BUILD_UNIX_TIME = 1775245992083L;
+   public static final String BUILD_DATE = "2026-04-03 15:58:22 EDT";
+   public static final long BUILD_UNIX_TIME = 1775246302975L;
    public static final int DIRTY = 0;
 
    private BuildConstants(){}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1065,7 +1065,7 @@ public class RobotContainer {
 	private Command buildBackupRobotAimCommand() {
 		return Commands.startEnd(
 				() -> setBackupRobotAimingEnabled(true),
-				() -> setBackupRobotAimingEnabled(false))
+				() -> setBackupRobotAimingEnabled(true))
 				.alongWith(new AimToPose(
 						drivetrainS,
 						() -> new Pose2d(getBackupAimTargetTranslation(), new Rotation2d()),
