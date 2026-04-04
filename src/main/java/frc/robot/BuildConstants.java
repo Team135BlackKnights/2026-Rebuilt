2f021fb1d40f4b93e26337b12f211303f068692d
Eaglegrant <grantman522@gmail.com>
2026-04-04 11:21:28 -0500

    more vision trust?

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
    public static final String MAVEN_GROUP = "";
    public static final String MAVEN_NAME = "2026-Rebuilt";
    public static final String VERSION = "unspecified";
-   public static final int GIT_REVISION = 1185;
-   public static final String GIT_SHA = "1ffc99700feff70838597a50d5f389c787cf0b44";
-   public static final String GIT_DATE = "2026-04-04 09:55:52 EDT";
+   public static final int GIT_REVISION = 1188;
+   public static final String GIT_SHA = "650e3873c7f9a95b9dce6f99aa89771c81da1baa";
+   public static final String GIT_DATE = "2026-04-04 11:28:32 EDT";
    public static final String GIT_BRANCH = "event_2026wilax";
-   public static final String BUILD_DATE = "2026-04-04 09:55:54 EDT";
-   public static final long BUILD_UNIX_TIME = 1775310954081L;
-   public static final int DIRTY = 0;
+   public static final String BUILD_DATE = "2026-04-04 12:19:30 EDT";
+   public static final long BUILD_UNIX_TIME = 1775319570452L;
+   public static final int DIRTY = 1;
 
    private BuildConstants(){}
 }
