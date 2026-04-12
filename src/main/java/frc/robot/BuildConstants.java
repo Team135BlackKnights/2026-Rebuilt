46c49b748699f4ee1f4508c5a9a23b9178536099
driverstation1 <robotics@phm.k12.in.us>
2026-04-11 21:02:48 -0400

    amps good

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
    public static final String MAVEN_GROUP = "";
    public static final String MAVEN_NAME = "2026-Rebuilt";
    public static final String VERSION = "unspecified";
-   public static final int GIT_REVISION = 1159;
-   public static final String GIT_SHA = "f2fdcdba783bbbdd64eb53ba14227aa407410bf0";
-   public static final String GIT_DATE = "2026-04-11 11:11:32 EDT";
+   public static final int GIT_REVISION = 1160;
+   public static final String GIT_SHA = "1aaed196af6389dd33cb4393c041afd69a229a76";
+   public static final String GIT_DATE = "2026-04-11 16:44:20 EDT";
    public static final String GIT_BRANCH = "2026-Robot-Code-V2";
-   public static final String BUILD_DATE = "2026-04-11 16:21:02 EDT";
-   public static final long BUILD_UNIX_TIME = 1775938862724L;
+   public static final String BUILD_DATE = "2026-04-11 16:52:32 EDT";
+   public static final long BUILD_UNIX_TIME = 1775940752403L;
    public static final int DIRTY = 1;
 
    private BuildConstants(){}
