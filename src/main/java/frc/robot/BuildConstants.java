83e0c42e5974f632844ef8945e62c71627235167
Vazgen Manukyan <vmanukyan135@phm.k12.in.us>
2026-04-18 10:30:41 -0400

    Update at 'Sat Apr 18 10:30:40 EDT 2026'

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
    public static final String MAVEN_GROUP = "";
    public static final String MAVEN_NAME = "2026-Rebuilt";
    public static final String VERSION = "unspecified";
-   public static final int GIT_REVISION = 1209;
-   public static final String GIT_SHA = "6986d3b2f1d9b544e84a2d07f1c3cfe0ca0c0058";
-   public static final String GIT_DATE = "2026-04-18 10:25:52 EDT";
+   public static final int GIT_REVISION = 1210;
+   public static final String GIT_SHA = "395af754b58e03de5aaea84160e23827bf116fef";
+   public static final String GIT_DATE = "2026-04-18 10:28:59 EDT";
    public static final String GIT_BRANCH = "event_2026incmp";
-   public static final String BUILD_DATE = "2026-04-18 10:25:54 EDT";
-   public static final long BUILD_UNIX_TIME = 1776522354560L;
+   public static final String BUILD_DATE = "2026-04-18 10:29:01 EDT";
+   public static final long BUILD_UNIX_TIME = 1776522541836L;
    public static final int DIRTY = 0;
 
    private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/Turret/hood/HoodIOKrakenFOC.java
+++ src/main/java/frc/robot/subsystems/Turret/hood/HoodIOKrakenFOC.java
@@ -106,7 +106,7 @@ public class HoodIOKrakenFOC implements HoodIO {
         talon = new TalonFXS(motorID, bus);
 
         cfg.Commutation.MotorArrangement = MotorArrangementValue.Brushed_DC;
-        cfg.MotorOutput.Inverted = isLeft ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive;
+        cfg.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
         cfg.Commutation.BrushedMotorWiring = BrushedMotorWiringValue.Leads_A_and_B;
 
         cfg.ExternalTemp.TempSensorRequired = TempSensorRequiredValue.Not_Required;
