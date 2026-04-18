0e3e9fde98666138380d56f93afd6ef928e2c089
Eaglegrant <grantman522@gmail.com>
2026-04-18 11:04:11 -0400

    both hoods

--- src/main/java/frc/robot/subsystems/Turret/hood/HoodIOKrakenFOC.java
+++ src/main/java/frc/robot/subsystems/Turret/hood/HoodIOKrakenFOC.java
@@ -189,7 +189,7 @@ public class HoodIOKrakenFOC implements HoodIO {
 
         double volts = ff.calculate(sensorRps) + controller.calculate(sensorRot);
 
-        if (!isLeft)runVolts(volts);
+        runVolts(volts);
 
         inputs.positionRads = sensorRotToHoodRad(sensorRot);
         inputs.velocityRadsPerSec = Units.rotationsToRadians(sensorRps);
@@ -206,8 +206,7 @@ public class HoodIOKrakenFOC implements HoodIO {
         if (zeroingActive) return;
 
         double clamped = MathUtil.clamp(positionRads, minAngleRads, maxAngleRads);
-        if (!(isLeft)) 
-            controller.setSetpoint(hoodRadToSensorRot(clamped));
+        controller.setSetpoint(hoodRadToSensorRot(clamped));
     }
 
     @Override
