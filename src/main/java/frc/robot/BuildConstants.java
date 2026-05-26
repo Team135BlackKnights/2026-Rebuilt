9709caf5d66d802313525372c504bda8be20ee3b
marsh135 <86257034+marsh135@users.noreply.github.com>
2026-05-26 12:55:40 -0400

    fixing build constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
    public static final String MAVEN_GROUP = "";
    public static final String MAVEN_NAME = "2026-Rebuilt";
    public static final String VERSION = "unspecified";
-   public static final int GIT_REVISION = 1194;
-   public static final String GIT_SHA = "f6d3f90170743c345987b3ed7fc1c6169c135337";
-   public static final String GIT_DATE = "2026-04-04 14:56:48 EDT";
+   public static final int GIT_REVISION = 1197;
+   public static final String GIT_SHA = "f5ff009a863a94b30e00bef6171d0b7dabdebe43";
+   public static final String GIT_DATE = "2026-04-17 18:50:01 EDT";
    public static final String GIT_BRANCH = "event_2026wilax";
-   public static final String BUILD_DATE = "2026-04-04 14:58:09 EDT";
-   public static final long BUILD_UNIX_TIME = 1775329089497L;
-   public static final int DIRTY = 0;
+   public static final String BUILD_DATE = "2026-05-22 13:24:19 EDT";
+   public static final long BUILD_UNIX_TIME = 1779470659475L;
+   public static final int DIRTY = 1;
 
    private BuildConstants(){}
 }
